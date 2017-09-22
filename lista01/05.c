bee79eadb44206b27de28e9fca625d24f33a80dd
ufabc <adriano.shimonoe@gmail.com>
2017-09-22 09:57:27 -0300

    22/09/2017 - Resposta do exercício.

---
+++ lista01/05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/* Divisão de dois valores inteiros */
+int main() {
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    printf("%d\n", x / y);
+
+    return 0;
+}
