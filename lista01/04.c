d2791fccc326741d6afdbca00ec96e5953c14ef2
ufabc <adriano.shimonoe@gmail.com>
2017-09-22 09:53:56 -0300

    22/09/2017 - Resposta do exercício.

---
+++ lista01/04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/* Multiplicação de dois valores inteiros */
+int main() {
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    printf("%d\n", x * y);
+
+    return 0;
+}
