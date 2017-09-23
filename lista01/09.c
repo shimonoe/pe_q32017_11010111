29228cdefb2f0aac5617113c3ebf5102cb27da1d
Adriano Shimonoe Moreno dos Santos <adriano.shimonoe@gmail.com>
2017-09-23 10:59:10 -0300

    23/09/2017 - Resposta do exercício.     - Problema na função de conversão de Celcius para Fahrenheit:         -> Mesmo com o casting de double o cálculo não é feito corretamente.

---
+++ lista01/09.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int celciusParaFahrenheit(double celcius);
+int fahrenheitParaCelcius(double fahrenheit);
+
+/* Faz a conversão de uma temperatura para Celcius ou Fahrenheit dependendo da opção */
+int main() {
+    double temperatura, convertido;
+    int opcao;
+
+    scanf("%lf", &temperatura);
+    scanf("%d", &opcao);
+
+    if ( opcao == 1 ) {
+        convertido = celciusParaFahrenheit(temperatura);
+    } else {
+        convertido = fahrenheitParaCelcius(temperatura);
+    }
+
+    printf("%.2lf\n", convertido);
+
+    return 0;
+}
+
+int celciusParaFahrenheit(double celcius) {
+    double fahrenheit;
+    fahrenheit =  celcius * 1.80 + 32.00;
+    return fahrenheit;
+}
+
+int fahrenheitParaCelcius(double fahrenheit) {
+    double celcius;
+    celcius = fahrenheit - 32.00;
+    celcius /= 1.80;
+    return celcius;
+}
